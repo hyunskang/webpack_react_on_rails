823d5549c92e72654487d81b4ae00cf0be376072
brokenfingers <hskang@dotandbo.com>
2016-08-22 23:28:54 -0700

    need extra indentation

--- lib/generators/react_redux_webpack_scaffolder/scaffold_generator.rb
+++ lib/generators/react_redux_webpack_scaffolder/scaffold_generator.rb
@@ -28,54 +28,54 @@ module ReactReduxWebpackScaffolder
     def application_details
       <<-APP_DETAILS.strip_heredoc
   "name": "#{rails_application_name}",
-  "description": "TODO: Add a brief description for the application",
-  "version": "1.0.0",
+    "description": "TODO: Add a brief description for the application",
+    "version": "1.0.0",
       APP_DETAILS
     end
 
     def babel_details
       <<-BABEL_DETAILS.strip_heredoc
   "babel": {
-    "presets": [
-      "es2015",
-      "react"
-    ],
-    "plugins": [
-      "transform-object-assign"
-    ]
-  },
+      "presets": [
+        "es2015",
+        "react"
+      ],
+      "plugins": [
+        "transform-object-assign"
+      ]
+    },
       BABEL_DETAILS
     end
 
     def dev_dependencies
       <<-DEV_DEPENDENCIES.strip_heredoc
   "devDependencies": {
-    "babel-core": "^6.13.2",
-    "babel-loader": "^6.2.5",
-    "babel-preset-es2015": "^6.13.2",
-    "babel-preset-react": "^6.11.1",
-    "exports-loader": "~0.6.2",
-    "expose-loader": "~0.6.0",
-    "imports-loader": "~0.6.3",
-    "lodash": "~2.4.1",
-    "react-hot-loader": "^1.3.0",
-    "webpack": "~1.4.13"
-  },
+      "babel-core": "^6.13.2",
+      "babel-loader": "^6.2.5",
+      "babel-preset-es2015": "^6.13.2",
+      "babel-preset-react": "^6.11.1",
+      "exports-loader": "~0.6.2",
+      "expose-loader": "~0.6.0",
+      "imports-loader": "~0.6.3",
+      "lodash": "~2.4.1",
+      "react-hot-loader": "^1.3.0",
+      "webpack": "~1.4.13"
+    },
       DEV_DEPENDENCIES
     end
 
     def dependencies
       <<-DEPENDENCIES.strip_heredoc
   "dependencies": {
-    "babel-plugin-transform-object-assign": "^6.8.0",
-    "immutable": "^3.8.1",
-    "isomorphic-fetch": "^2.2.1",
-    "react": "^15.3.1",
-    "react-dom": "^15.3.1",
-    "react-redux": "^4.4.5",
-    "redux": "^3.5.2",
-    "redux-thunk": "^2.1.0"
-  }
+      "babel-plugin-transform-object-assign": "^6.8.0",
+      "immutable": "^3.8.1",
+      "isomorphic-fetch": "^2.2.1",
+      "react": "^15.3.1",
+      "react-dom": "^15.3.1",
+      "react-redux": "^4.4.5",
+      "redux": "^3.5.2",
+      "redux-thunk": "^2.1.0"
+    }
       DEPENDENCIES
     end
 
